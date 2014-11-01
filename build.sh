b57e2f8bf8bb1419d9e7dbff4120dbbbeef38eec
Alan Kent <alan.james.kent@gmail.com>
2014-11-01 04:41:45 +0000

    Alpha102

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker build -t docker-magento2-demo-apache .
 
 echo To publish use:
 echo docker tag docker-magento2-demo-apache alankent/docker-magento2-demo-apache
-echo docker tag docker-magento2-demo-apache alankent/docker-magento2-demo-apache:0.1.0-alpha101
-echo docker push alankent/docker-magento2-demo-apache:0.1.0-alpha101
+echo docker tag docker-magento2-demo-apache alankent/docker-magento2-demo-apache:0.1.0-alpha102
+echo docker push alankent/docker-magento2-demo-apache:0.1.0-alpha102
